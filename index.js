5ed4507fd603f41ff71399eda24f4ffe9826e9ae
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-10-04 04:32:41 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+
+add(2,3);
+subtract(2,3);
+multiply(2,3);
+divide(2,3);
+inc(2,3);
+dec(2,3);
+
+function add(a, b){
+  return a+b;
+}
+function subtract(a, b){
+  return a-b;
+}
+function multiply(a, b){
+  return a*b;
+}
+function divide(a, b){
+  return a/b;
+}
+function inc(n){
+  return n++;
+}
+function dec(n){
+  return n--;
+}
